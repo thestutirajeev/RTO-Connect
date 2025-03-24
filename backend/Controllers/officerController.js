be172e8ba0a3f8b1167323044dbc070096cb07f6
Stuti Rajeev <stutirajeev5@gmail.com>
2025-03-24 20:46:45 +0530

    Controllers

--- backend/Controllers/officerController.js
+++ backend/Controllers/officerController.js
@@ -1,6 +1,6 @@
 import { query } from "express";
 import Officer from "../models/OfficerSchema.js";
-//PUT
+// PUT
 export const updateOfficer = async (req, res) => {
     const id = req.params.id;
 
@@ -20,7 +20,7 @@ export const updateOfficer = async (req, res) => {
         res.status(500).json({ success: false, message: "Failed to update Officer"});
     } 
 };
-//DELETE
+// DELETE
 export const deleteOfficer = async (req, res) => {
     const id = req.params.id;
 
@@ -35,7 +35,7 @@ export const deleteOfficer = async (req, res) => {
         res.status(500).json({ success: false, message: "Failed to delete Officer"});
     } 
 };
-//GET
+// GET
 export const getSingleOfficer = async (req, res) => {
     const id = req.params.id;
 
@@ -53,18 +53,29 @@ export const getSingleOfficer = async (req, res) => {
         res.status(404).json({ success: false, message: "No Officer Found"});
     } 
 };
-//GET
+// GET
 // GET all officers
 export const getAllOfficer = async (req, res) => {
-    try {
-        const officers = await Officer.find().select("-password");
+    try{
+        const {query} = req.query;
+        let officers;
+        if(query){
+            officers = await Officer.find({'isApproved': 'approved', 
+                $or:[
+                    {name:{$regex:query, $options: 'i'}}, 
+                ]
+            }).select("-password");
+        }
+        else{
+            officers = await Officer.find({'isApproved': 'approved'}).select("-password");
+        }
 
         res.status(200).json({
             success: true,
             message: "Officers Found",
             data: officers,
         });
-    } catch (err) {
-        res.status(404).json({ success: false, message: "Officers Not Found" });
-    }
+    }catch(err){
+        res.status(404).json({ success: false, message: "Not Found"});
+    } 
 };
