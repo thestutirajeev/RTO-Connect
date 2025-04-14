b6be2c25808e6d5e87d7c34be156ba5128dc20a2
Stuti Rajeev <stutirajeev5@gmail.com>
2025-04-14 21:58:57 +0530

    Routes redefined

--- frontend/src/routes/Routers.jsx
+++ frontend/src/routes/Routers.jsx
@@ -6,11 +6,6 @@ import Contact from "../pages/Contact";
 import Dashboard from "../pages/Dashboard";
 
 import AdminHome from "../pages/Admin/AdminHome";
-import VehicleList from "../pages/Admin/VehicleList";
-import ViewApplication from "../pages/Admin/ViewApplication";
-import UserList from "../pages/Admin/UserList";
-import FeedbackList from "../components/Feedbacks/FeedbackList";
-import ContactedQueriesList from "../pages/Admin/ContactedQueriesList"; 
 
 import UserHome from "../pages/User/UserHome";
 import VehicleRegistration from "../pages/User/VehicleRegistration";
@@ -37,12 +32,7 @@ const Routers = () => {
 
     {/* Admin Routes */}
     <Route path="/admin-home" element={<AdminHome />} />
-    <Route path="/vehicle-list" element={<VehicleList />} />
-    <Route path="/view-applications" element={<ViewApplication />} />
-    <Route path="/user-list" element={<UserList />} />
-    <Route path="/feedback-list" element={<FeedbackList />} />
-    <Route path="/contacts-list" element={<ContactedQueriesList />} />
-
+  
     {/* User Routes */}
 
     <Route path="/user-home" element={<UserHome />} />
